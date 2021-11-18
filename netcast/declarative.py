1edf567cc863e36f9a0ce30b536211d7af0f6cca
Stim <spcstim@gmail.com>
2021-11-18 23:58:44 +0100

    Add declarative API

--- netcast/declarative.py
+++ netcast/declarative.py
@@ -1,36 +1,56 @@
+import inspect
 import types
-from typing import Type, TypeVar, Sequence
+from typing import Type, TypeVar, Sequence, Any
 
 import construct as cs
 
 IGNORE_ATTRS = (
-    '__repr__', '__hash__', '__str__', '__getattribute__', '__setattr__',
-    '__delattr__', '__lt__', '__le__', '__eq__', '__ne__', '__gt__',
+    '__setup__', '__validate__',
+
+    '__dict__', '__weakref__', '__repr__', '__hash__', '__str__', '__getattribute__',
+    '__setattr__', '__delattr__', '__lt__', '__le__', '__eq__', '__ne__', '__gt__',
     '__ge__', '__init__', '__new__', '__reduce_ex__', '__reduce__',
     '__subclasshook__', '__init_subclass__', '__format__', '__sizeof__',
     '__dir__', '__class__', '__doc__', '__module__'
 )
+
 C = TypeVar('C', bound=cs.Construct)
-M = object()
+
+
+class _Missing:
+    def __repr__(self):
+        return 'Missing'
+
+
+M = _Missing()
 
 
 _main_registry = {
     'constructs': {},
     'factories': {},
     'aliased_args': {},
-    'reserve_args': {}
+    'reserve_args': {},
+    'field_reg': {}
 }
 
 
-def _create_registry_api(key, default=M, raise_for_missing=False, registry=None):
+def _create_registry_api(
+        key,
+        default: Any = M,
+        create_if_missing=True,
+        raise_for_missing=False,
+        registry=None
+):
     if registry is None:
         registry = _main_registry
 
     def _get(obj, quiet_if_missing=False):
         raise_missing_here = raise_for_missing and not quiet_if_missing
         v = registry[key].get(id(obj), default)
+        if create_if_missing and v is default:
+            registry[key][id(obj)] = v
         if v is default and raise_missing_here:
-            raise ValueError(f'{key} is missing')
+            raise ValueError(f'value from {key} registry is missing')
         return v
 
     def _set(obj, value):
@@ -43,6 +63,7 @@ get_factory, set_factory = _create_registry_api('factories', raise_for_missing=T
 get_construct, set_construct = _create_registry_api('constructs', raise_for_missing=True)
 get_reserve_args, set_reserve_args = _create_registry_api('reserve_args', default={})
 get_aliased_args, set_aliased_args = _create_registry_api('aliased_args', default={})
+get_field_reg, set_field_reg = _create_registry_api('field_reg', default={})
 
 
 def _ensure_type(obj):
@@ -77,6 +98,9 @@ def _finalize_args(cls, args=None):
             set_name = get_alias(name)
         final_args[set_name] = field = wrap_field(value, name)
         setattr(cls, name, field)
+        field_reg = get_field_reg(cls)
+        field_reg[name] = field
+        set_field_reg(cls, field_reg)
     return final_args
 
 
@@ -84,7 +108,7 @@ def _subclass_hook(cls):
     cls.__setup__ and set_construct(cls, get_factory(cls.mro()[1])(**_finalize_args(cls)))
 
 
-def _subclass(cls, args):
+def _spawn_subclass(cls, args):
     subcls = type(cls.__name__, (cls,), {'__setup__': False})
     set_factory(subcls, get_factory(cls))
     set_construct(subcls, get_factory(cls)(**_finalize_args(cls, args=args)))
@@ -92,7 +116,10 @@ def _subclass(cls, args):
 
 
 def serialize_from_object(decl, obj, **context_kwds):
-    obj = {get_fields(obj)}
+    obj = {
+        k: v for k, v in {**get_fields(decl, final=True), **obj}.items()
+        if v is not M
+    }
     return get_construct(_ensure_type(decl)).build(obj, **context_kwds)
 
 
@@ -107,19 +134,19 @@ def reinterpret(decl, byte_string, **context_kwds):
     return decl
 
 
-def port(
+def create_decl(
         factory: Type[C],
         reserve_args: Sequence[str] = (),
         **custom_reserve_args: str
 ):
     reserve_args = {name: f'__{name}__' for name in reserve_args}
-    for name, reservation in custom_reserve_args.items():
+    for name, reserved_arg in custom_reserve_args.items():
         if name in reserve_args:
             raise ValueError(f'double-passed {name!r} in reserved args')
-        reserve_args[name] = reservation
+        reserve_args[name] = reserved_arg
     cls = type(
         factory.__name__,
-        (ConstructAttributeTree,),
+        (DeclarativeConstruct,),
         {'__init_subclass__': _subclass_hook}
     )
     set_factory(cls, factory)
@@ -127,19 +154,23 @@ def port(
     return cls
 
 
-def get_fields(cls, aliased_form=False):
-    d = cls if isinstance(cls, dict) else vars(_ensure_type(cls))
+def get_fields(cls, aliased_form=False, final=False):
+    if final:
+        aliased_form = True
+    field_reg = get_field_reg(cls)
+    if field_reg:
+        d = dict(field_reg)
+    else:
+        d = cls if isinstance(cls, dict) else {k: getattr(cls, k) for k in dir(cls)}
     return {
         (
-            get_alias(k) if
-            k in get_aliased_args(cls)
+            get_alias(k)
+            if k in get_aliased_args(cls)
             and aliased_form
             else k
-        ): v for k, v in d.items()
-        if (
-            k not in IGNORE_ATTRS
-            and not isinstance(v, types.FunctionType)
-        )
+        ): v.get(cls) if final else v
+        for k, v in d.items()
+        if k not in IGNORE_ATTRS
     }
 
 
@@ -186,12 +217,13 @@ class FieldDescriptor(cs.Subconstruct):  # noqa
         if value is M:
             if isinstance(self.subcon, cs.Const):
                 return self.subcon.value
-            if self.default is M:
-                raise UnboundFieldError(self.name)
+            # if self.default is M:
+            #     raise UnboundFieldError(self.name)
             return self.default
         return value
 
     def __set__(self, instance, value):
+        print('set', instance, value)
         self.set(instance, self.validate(instance, value))
 
     def __truediv__(self, other):
@@ -199,17 +231,17 @@ class FieldDescriptor(cs.Subconstruct):  # noqa
     __rtruediv__ = __truediv__
 
 
-class ConstructAttributeTree:
+class DeclarativeConstruct:
     __setup__ = True
     __validate__ = True
 
     def __new__(cls, **args):
         if get_construct(cls, quiet_if_missing=True) is M:
-            return _subclass(cls, args)
+            return _spawn_subclass(cls, args)
         elif args:
             fields = get_fields(cls)
             if not fields:
-                return _subclass(cls, args)
+                return _spawn_subclass(cls, args)
             self = object.__new__(cls)
             for key, val in args.items():
                 if key not in fields:
@@ -220,13 +252,13 @@ class ConstructAttributeTree:
 
     def __repr__(self):
         cls_name = type(self).__name__
-        fields_fmt = ', '.join(f'{k}={v!r}' for k, v in get_fields(self).items())
+        fields_fmt = ', '.join(f'{k}={v!r}' for k, v in get_fields(self, final=True).items())
         return cls_name + f'({fields_fmt})'
 
 
-Struct = port(cs.Struct)
-Sequence = port(cs.Sequence)
-FocusedSeq = port(cs.FocusedSeq, reserve_args=['parsebuildfrom'])
-Union = port(cs.Union, reserve_args=['parsefrom'])
-Select = port(cs.Select)
-LazyStruct = port(cs.LazyStruct)
+Struct = create_decl(cs.Struct)
+Sequence = create_decl(cs.Sequence)
+FocusedSeq = create_decl(cs.FocusedSeq, reserve_args=['parsebuildfrom'])
+Union = create_decl(cs.Union, reserve_args=['parsefrom'])
+Select = create_decl(cs.Select)
+LazyStruct = create_decl(cs.LazyStruct)
