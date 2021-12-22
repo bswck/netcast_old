9371599a4d0e7b80d88f6cfc5a1bf219e7f4cfe8
Stim <spcstim@gmail.com>
2021-12-22 11:30:09 +0100

    Fix Replacement.from_multiple_replacements

--- netcast/conversions.py
+++ netcast/conversions.py
@@ -2,11 +2,11 @@ from __future__ import annotations
 
 import dataclasses
 import operator
-from typing import Any, Hashable, Callable, TypeVar
+from typing import Any, Hashable, Callable, TypeVar, Sequence
 
 
 def coerce_var(v):
-    if isinstance(v, Var):
+    if isinstance(v, _BaseVar):
         return v
     return Var(v)
 
@@ -15,20 +15,51 @@ TT = TypeVar('TT', Callable[[Any, Any], Any], type(None))
 
 
 @dataclasses.dataclass
-class Var:
-    value: Hashable
-    transformer: TT = None
-    metadata: dict | None = None
+class Replacement:
+    from_value: _BaseVar
+    to_value: _BaseVar
+    key: KT = operator.eq
+
+    def __new__(cls, *args, **kwargs):
+        if args:
+            maybe_replacement = args[0]
+            if isinstance(maybe_replacement, Replacement):
+                return maybe_replacement
+        return object.__new__(cls)
+
+    @classmethod
+    def from_multiple_replacements(
+            cls, *replacements: (
+                Sequence[_BaseVar, _BaseVar]
+                | Sequence[_BaseVar, _BaseVar, KT]
+                | Replacement
+            )
+    ):
+        return tuple(map(lambda r: (cls, lambda _: r)[isinstance(r, cls)](*r), replacements))
+
+
+class _BaseVar:
+    _replacement_class = Replacement
 
     def __neg__(self):
+        """Used for left-arrow notation."""
         return self
 
     def __lt__(self, other):
-        return Replacement(coerce_var(other), self)
+        """Used for left-arrow notation."""
+        return self._replacement_class(coerce_var(other), self)
 
 
 @dataclasses.dataclass
-class Replacement:
-    from_value: Var
-    to_value: Var
-    key: Callable[[Var, Var], bool] = operator.eq
+class Var(_BaseVar):
+    value: Hashable
+    transformer: TT = None
+
+
+@dataclasses.dataclass
+class MemoryVar(_BaseVar):
+    value: Any
+    transformer = id
+
+
+KT = Callable[[_BaseVar, _BaseVar], bool]
