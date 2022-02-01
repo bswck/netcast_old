152045b707ac0a87a9e8bb72d0b311cfd87bd224
stijm <spcstim@gmail.com>
2022-02-01 10:22:12 +0100

    Add kwargs support for AsyncSerializer cast methods

--- netcast/cast/plugins/async_serializer.py
+++ netcast/cast/plugins/async_serializer.py
@@ -2,18 +2,18 @@ from netcast.cast.plugin import default, feature, Plugin
 
 
 class AsyncSerializer(Plugin):
-    async def _default_cast_async(self, load_or_dump):
+    async def _default_cast_async(self, load_or_dump, **kwargs):
         raise NotImplementedError
 
     _dump_async = default(_default_cast_async)
     _load_async = default(_default_cast_async)
 
     @feature
-    async def load_async(self, dumped, context=None):
+    async def load_async(self, dumped, **kwargs):
         """Cast an origin value to the cast type."""
-        return await self._load_async(dumped, context=None)
+        return await self._load_async(dumped, **kwargs)
 
     @feature
-    async def dump_async(self, loaded):
+    async def dump_async(self, loaded, **kwargs):
         """Cast an origin value to the cast type."""
-        return await self._dump_async(loaded)
+        return await self._dump_async(loaded, **kwargs)
